f904699d94728c283bd20fa65687e71c9497b787
Maleklabbaci <Marcolabbaci@gmail.com>
2026-06-25 08:56:52 +0100

    Wrap App component with ToastProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ToastProvider } from './contexts/ToastContext';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </StrictMode>,
 );
